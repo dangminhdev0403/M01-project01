0eefd842d091da148b425ccd74ea40f4c2074756
Dang Hoang Minh <minhphp2005@gmail.com>
2024-08-29 21:40:46 +0700

    add animatie

---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+            ScrollReveal().reveal('.widget2', {
+                origin: 'bottom',
+                distance: '20px',
+                duration: 600,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+
+            ScrollReveal().reveal('.heading-left', {
+                origin: 'left',
+                distance: '200px',
+                duration: 1000,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+            ScrollReveal().reveal('.heading-left-lv2', {
+                origin: 'left',
+                distance: '200px',
+                duration: 1500,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+            ScrollReveal().reveal('.heading-right', {
+                origin: 'right',
+                distance: '200px',
+                duration: 1000,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+            ScrollReveal().reveal('.heading-right-lv2', {
+                origin: 'right',
+                distance: '200px',
+                duration: 1500,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+
+            ScrollReveal().reveal('.heading-right-lv3', {
+                origin: 'right',
+                distance: '200px',
+                duration: 3000,
+                easing: 'ease-in-out',
+                interval: 100,
+            });
+
+
+        });
