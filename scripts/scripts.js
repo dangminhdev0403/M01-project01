730b6971216d286996838d612a8dcbe96665feee
Dang Hoang Minh <minhphp2005@gmail.com>
2024-08-30 23:59:29 +0700

    succes feedback to popular2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,49 +1,41 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-            ScrollReveal().reveal('.widget2', {
-                origin: 'bottom',
-                distance: '20px',
-                duration: 600,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('.widget2-left', {
+        origin: 'left',
+        distance: '345px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.widget2-left-lv2', {
+        origin: 'left',
+        distance: '345px',
+        duration: 1500,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.widget2-right', {
+        origin: 'right',
+        distance: '345px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.widget2-right-lv2', {
+        origin: 'right',
+        distance: '345px',
+        duration: 1500,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
+    ScrollReveal().reveal('.widget2-bottom', {
+        origin: 'bottom',
+        distance: '345px',
+        duration: 1500,
+        easing: 'ease-in-out',
+        interval: 100,
+    });
 
-            ScrollReveal().reveal('.heading-left', {
-                origin: 'left',
-                distance: '200px',
-                duration: 1000,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
-            ScrollReveal().reveal('.heading-left-lv2', {
-                origin: 'left',
-                distance: '200px',
-                duration: 1500,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
-            ScrollReveal().reveal('.heading-right', {
-                origin: 'right',
-                distance: '200px',
-                duration: 1000,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
-            ScrollReveal().reveal('.heading-right-lv2', {
-                origin: 'right',
-                distance: '200px',
-                duration: 1500,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
+   
 
-            ScrollReveal().reveal('.heading-right-lv3', {
-                origin: 'right',
-                distance: '200px',
-                duration: 3000,
-                easing: 'ease-in-out',
-                interval: 100,
-            });
-
-
-        });
+});
